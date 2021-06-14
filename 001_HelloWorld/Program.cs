3cb9176cb10a9b203bf4f0366018da1ec2c2a603
hwalang <smdong97@gmail.com>
2021-06-14 23:30:09 +0900

    초기 커밋.

---
+++ 001_HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: _001_HelloWorld
+ * DESC: 코드리뷰
+ * 
+ * static void Main: 최초의 진입점(Entry Point)
+ */
+
+namespace _001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow Wolrd");
+        }
+    }
+}
