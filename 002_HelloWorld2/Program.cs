d5e9b623651366e54741f54be29a3d75e0e65267
hwalang <smdong97@gmail.com>
2021-06-14 23:40:05 +0900

    c# 1. args 에 값을 넣는 방법

---
+++ 002_HelloWorld2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: 002_HelloWorld2
+ * DESC: 메인 함수의 파라미터 보기
+ * 
+ *              <args에 값을 넣고 싶다면>
+ *              
+ * 002_HelloWorld2 -> bin -> debug 경로를 저장한 후
+ * PracticeCS 폴더에서 프롬프트 창 열기
+ *  1. cd..
+ *  2. cd 저장한 경로 붙여넣기
+ *  3. dir
+ *  4. 002_HelloWorld2.exe 10(args에 저장한 파라미터)
+ *  5. 'HelloWorld: 10' 이 출력된다.
+ * 
+ */
+
+namespace _002_HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                Console.WriteLine("HelloWorld:" + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("HelloWorld: args.Length = 0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
